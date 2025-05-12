7bd04c8afc4fadf8193ffaa54c63ebe611ebf6ad
Devpatel599 <devpatelap2345@gmail.com>
2025-05-11 23:24:42 -0500

    commit note
    Addons - Added the Junit and the tests added for board and other things.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
+
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,13 +1,57 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AppTest {
+public class AppTest {
+
+    private Board board;
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+    }
+
     @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest, "app should be initialized");
+    void testBoardInitialState() {
+        for (int i = 1; i <= 9; i++) {
+            assertTrue(board.isCellEmpty(i), "Cell " + i + " should be initially empty");
+        }
+    }
+
+    @Test
+    void testPlaceMoveAndWinner() {
+        board.placeMove(1, 'X');
+        board.placeMove(2, 'X');
+        board.placeMove(3, 'X');
+        assertTrue(board.isWinner('X'), "Player X should be the winner");
+    }
+
+    @Test
+    void testNoWinnerOnEmptyBoard() {
+        assertFalse(board.isWinner('X'), "No winner expected on a new board");
+    }
+
+    @Test
+    void testComputerPlayerFirstMoveCorner() {
+        ComputerPlayer cpu = new ComputerPlayer();
+        int move = cpu.chooseMove(board, 'X', 'O', 0);
+        assertTrue(move == 1 || move == 3 || move == 7 || move == 9, "First move should be a corner");
+    }
+
+    @Test
+    void testGameEndsInTie() {
+        board.placeMove(1, 'X');
+        board.placeMove(2, 'O');
+        board.placeMove(3, 'X');
+        board.placeMove(4, 'X');
+        board.placeMove(5, 'O');
+        board.placeMove(6, 'X');
+        board.placeMove(7, 'O');
+        board.placeMove(8, 'X');
+        board.placeMove(9, 'O');
+        assertFalse(board.isWinner('X'));
+        assertFalse(board.isWinner('O'));
     }
 }
-
